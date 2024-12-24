780a0434647fb7a920f5628422c5556e7b77ed3e
MrVerdigris <max.abrashov.off@gmail.com>
2024-12-24 15:55:51 +0500

    Main commit

---
+++ example/graalvm-javafx-sharedlib/graalvm-javafx-sharedlib-display/src/main/java/ru/axalit/graalvm_javafx_sharedlib/display/AxDemoLaunch.java
@@ -0,0 +1,15 @@
+package ru.axalit.graalvm_javafx_sharedlib.display;
+
+public class AxDemoLaunch {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("\n\tAXALIT DEMO LAUNCH\n\n");
+            AxDemoApplication.launch(AxDemoApplication.class, args);
+        } catch (Exception e) {
+            System.out.println("\n\tAXALIT DEMO LAUNCH FAILED\n\n");
+            e.printStackTrace();
+        }
+    }
+
+}
